d5997e4a8124f2719b21dee4ba1ee273095c7f05
kampn2687 <kampn2687@HRH-IC0027343.wrdsb.ca>
2015-10-20 11:19:17 -0400

    Dfs

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,6 @@
+
+import java.awt.Color;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -13,6 +16,28 @@ public class Game {
      * @param args the command line arguments
      */
     public static void main(String[] args) {
-        // TODO code application logic here
+        int row = (int)(Math.random() *8);
+        int col = (int)(Math.random() *8);
+        Doctor who = new Doctor (row,col);
+        GameBoard game = new GameBoard();
+        game.putPiece(row,col,Color.GREEN);
+    while(true){
+            // wait for user to click
+        Coordinate c = game.getClick();
+        game.removePiece(row,col);        
+        //figure out where they click
+        row = c.getRow();
+        col = c.getCol();
+        
+        //put piece
+        
+        who.move(row, col);
+        row = who.getRow();
+        col = who.getCol();
+    
+        game.putPiece(row,col,Color.GREEN);
+        
     }
+    }
+    
 }
