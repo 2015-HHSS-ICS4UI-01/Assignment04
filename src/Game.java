bf0bebbf0b706d52d0d5a04ba1fa51a1bd6ed6aa
fathn1690 <fathn1690@HRH-IC0027338.wrdsb.ca>
2015-10-22 11:14:54 -0400

    Added name to text file.

--- src/Game.java
+++ src/Game.java
@@ -1,3 +1,6 @@
+
+import java.awt.Color;
+
 /*
  * To change this template, choose Tools | Templates
  * and open the template in the editor.
@@ -8,11 +11,45 @@
  * @author lamonta
  */
 public class Game {
-
-    /**
-     * @param args the command line arguments
-     */
+    
     public static void main(String[] args) {
+        //initiates the doctors coordinates
+        int docRow = (int)(Math.random()*8); 
+        int docCol = (int)(Math.random()*8); 
+        
+        //initiates the daleks coordinates
+        int dalRow1 = (int)(Math.random()*8); 
+        int dalCol1 = (int)(Math.random()*8); 
+        int dalRow2 = (int)(Math.random()*8); 
+        int dalCol2 = (int)(Math.random()*8); 
+        int dalRow3 = (int)(Math.random()*8); 
+        int dalCol3 = (int)(Math.random()*8); 
+        
+        
+        GameBoard board = new GameBoard();
+        
+        Dalek one = new Dalek(dalRow1, dalCol1);
+        Dalek two = new Dalek(dalRow2, dalCol2);
+        Dalek three = new Dalek(dalRow3, dalCol3);
+        Doctor nima = new Doctor(docRow, docCol);
+              
+        
+        
+        
+        board.putPiece(nima.getRow(), nima.getCol() , Color.BLUE);
+        
+        
+        
+        board.putPiece(one.getRow(), one.getCol(), Color.YELLOW);
+        board.putPiece(dalRow2, dalCol2, Color.YELLOW);
+        board.putPiece(dalRow3, dalCol3, Color.YELLOW);
+        
+        Coordinate click = board.getClick();
+        nima.move(click.getRow(), click.getCol());
+        
+        board.putPiece(nima.getRow(), nima.getCol() , Color.BLUE);
+        
+        
         
         
     }
