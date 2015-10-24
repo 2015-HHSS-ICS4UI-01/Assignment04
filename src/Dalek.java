99ddc7583b4c9748751422fb9367c45f99a8f68d
Avelpa <game.player212@gmail.com>
2015-10-24 14:45:58 -0400

    Finished commenting the Dalek class

--- src/Dalek.java
+++ src/Dalek.java
@@ -7,49 +7,71 @@ import java.awt.Color;
  */
 
 /**
- *
+ * The enemy of the Doctor
  * @author kobed6328
  */
 public class Dalek {
     
+    // colors of the dalek in its dead and alive states
     private final Color CRASH_COLOR = Color.RED;
     private final Color ALIVE_COLOR = Color.BLACK;
+    private Color color;
     
     private Coordinate coordinate;
-    private Color color;
     private boolean crashed = false;
     
     public Dalek(Coordinate coordinate)
     {
         this.coordinate = coordinate;
+        // dalek should start as alive
         color = ALIVE_COLOR;
     }
-    
+    /**
+     * Returns the current color of the Dalek
+     * @return 
+     */
     public Color getColor()
     {
         return color;
     }
-    
+    /**
+     * Moves the Dalek to specified coordinates
+     * @param coordinate the coordinates to which the Dalek is moved
+     */
     public void moveTo(Coordinate coordinate)
     {
         this.coordinate = coordinate;
     }
-    
+    /**
+     * Gives the Dalek's current row number
+     * @return row
+     */
     public int getRow()
     {
         return coordinate.getRow();
     }
+    /**
+     * Gives the Dalek's current column number
+     * @return col
+     */
     public int getCol()
     {
         return coordinate.getCol();
     }
-    
+    /**
+     * Indicates whether or not the Dalek has crashed or not
+     * @return true, if the Dalek has crashed; otherwise, false
+     */
     public boolean hasCrashed()
     {
         return crashed;
     }
+    /**
+     * Crashes the current Dalek
+     */
     public void crash()
     {
+        // sets crashed state to true, and updates the Dale's color
         color = CRASH_COLOR;
         crashed = true;
     }
